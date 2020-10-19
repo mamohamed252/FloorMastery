95070abf5de3c54e7f7556d239156df23b5e7725
mamohamed252 <ma.mohamed252@gmail.com>
2020-10-19 02:03:39 -0500

    Finished DaoFile Unit testing

--- FloorMastery/src/test/java/com/mycompany/floormastery/DAO/FloorMasteryDAOFileImplTest.java
+++ FloorMastery/src/test/java/com/mycompany/floormastery/DAO/FloorMasteryDAOFileImplTest.java
@@ -21,74 +21,96 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author Mohamed
  */
 public class FloorMasteryDAOFileImplTest {
+
     FloorMasteryDAO testDAO;
-    
+
     public FloorMasteryDAOFileImplTest() {
     }
-    
-   
+
     @AfterAll
     public static void tearDownClass() {
     }
-    
+
     @BeforeEach
-    public void setUp()throws Exception {
+    public void setUp() throws Exception {
         String testFileLoc = "TestDAOFolder/Orders_10102020.txt";
-        
+
         FileWriter fw = new FileWriter(testFileLoc);
         fw.append("1,Ada Lovelace,CA,25.00,Tile,249.00,3.50,4.15,871.50,1033.35,476.21,2381.06");
         fw.flush();
         fw.close();
         testDAO = new FloorMasterDAOMock(testFileLoc);
     }
-    
+
     @AfterEach
     public void tearDown() {
     }
+
     @Test
-    public void testGetAllOrder() throws FloorMasteryDAOException{
-         //arrange
-      String date = "10102020";
-        OrderFile newOrder = new OrderFile(1,"test","TX", new BigDecimal("4.45"),"Wood", new BigDecimal("100"), new BigDecimal("4.75"), new BigDecimal("4.75"), new BigDecimal("475.00"), new BigDecimal("475.00"), new BigDecimal("42.28"), new BigDecimal("992.28"));
-         testDAO.addOrder(2, newOrder);
+    public void testGetAllOrder() throws FloorMasteryDAOException {
+        //arrange
+        String date = "10102020";
+        OrderFile newOrder = new OrderFile(1, "test", "TX", new BigDecimal("4.45"), "Wood", new BigDecimal("100"), new BigDecimal("4.75"), new BigDecimal("4.75"), new BigDecimal("475.00"), new BigDecimal("475.00"), new BigDecimal("42.28"), new BigDecimal("992.28"));
+        testDAO.addOrder(2, newOrder);
         //act
-         List<OrderFile> allOrders = testDAO.getAllOrders(date);
-         boolean doesContain = allOrders.contains(newOrder);
-        
+        List<OrderFile> allOrders = testDAO.getAllOrders(date);
+        boolean doesContain = allOrders.contains(newOrder);
+
         //assert
-        assertEquals(allOrders.size(),2, "Expected 2 orders");
+        assertEquals(allOrders.size(), 2, "Expected 2 orders");
         assertTrue(doesContain, "Expected order 1 to be test");
     }
+
     @Test
-    public void testRemoveOrder() {
-         //arrange
-        
-        //act
-        
-        //assert
-    }
-     @Test
-    public void testEditOrder() {
+    public void testRemoveOrder() throws FloorMasteryDAOException {
         //arrange
-        
-        //act
-        
-        //assert
-    }
-     @Test
-    public void testAddOrder()throws FloorMasteryDAOException  {
-         //arrange
         String date = "10102020";
-        OrderFile add = new OrderFile(1,"test","TX", new BigDecimal("4.45"),"Wood", new BigDecimal("100"), new BigDecimal("4.75"), new BigDecimal("4.75"), new BigDecimal("475.00"), new BigDecimal("475.00"), new BigDecimal("42.28"), new BigDecimal("992.28"));
+        OrderFile testOrder = new OrderFile(1, "Test", "CA", new BigDecimal("25.00"), "Tile", new BigDecimal("249.00"), new BigDecimal("3.50"), new BigDecimal("4.15"), new BigDecimal("871.50"), new BigDecimal("1033.35"), new BigDecimal("476.21"), new BigDecimal("2381.06"));
+        testDAO.addOrder(2, testOrder);
+        OrderFile removeOrder = testDAO.removeOrder(2, date);
+        //act
+        OrderFile removedOrder = testDAO.removeOrder(2, date);
+        OrderFile newOrder = testDAO.getUserOrder(2, date);
+        List<OrderFile> checkOrderRemoved = testDAO.getAllOrders(date);
+        Boolean doesContain = checkOrderRemoved.contains(testOrder);
+
+        //assert
+        assertEquals(newOrder, removedOrder, "Expected to remove testOrder order");
+        assertEquals(checkOrderRemoved.size(),1, "Expected 1 orders left");
+        //removed testOrder so it can not be in list.
+        assertEquals(doesContain, false, "Expected 1 orders left");
+    }
+
+    @Test
+    public void testEditOrder() throws FloorMasteryDAOException {
+        //arrange
+        String date = "10102020";
+        OrderFile testEdit= new OrderFile(1, "Test", "CA", new BigDecimal("25.00"), "Tile", new BigDecimal("249.00"), new BigDecimal("3.50"), new BigDecimal("4.15"), new BigDecimal("871.50"), new BigDecimal("1033.35"), new BigDecimal("476.21"), new BigDecimal("2381.06"));
+        OrderFile editOrder = testDAO.editOrder(1, testEdit, date);
+        //act
+        OrderFile editedOrder = testDAO.editOrder(1, editOrder, date);
+        OrderFile newOrder = testDAO.editOrder(1, editOrder, date);
+        List<OrderFile> checkEdit = testDAO.getAllOrders(date);
+        Boolean doesContain = checkEdit.contains(editOrder);
+        //assert
+         assertEquals(newOrder, editedOrder, "Expected to edit order number 1");
+         assertEquals(checkEdit.size(),1, "Expected file to contain ");
+         assertTrue(doesContain, "Expected order 1 to be test");
+    }
+
+    @Test
+    public void testAddOrder() throws FloorMasteryDAOException {
+        //arrange
+        String date = "10102020";
+        OrderFile add = new OrderFile(1, "test", "TX", new BigDecimal("4.45"), "Wood", new BigDecimal("100"), new BigDecimal("4.75"), new BigDecimal("4.75"), new BigDecimal("475.00"), new BigDecimal("475.00"), new BigDecimal("42.28"), new BigDecimal("992.28"));
         //act
         OrderFile testAdd = testDAO.addOrder(2, add);
         List<OrderFile> testList = testDAO.getAllOrders(date);
         Boolean doesContain = testList.contains(testAdd);
-        
-                
+
         //assert
-        assertEquals(testAdd.getOrderNumber(),2, "Expected order number to be 2");
+        assertEquals(testAdd.getOrderNumber(), 2, "Expected order number to be 2");
         assertTrue(doesContain, "Expected file to contain to testAdd");
-        
+
     }
 }
